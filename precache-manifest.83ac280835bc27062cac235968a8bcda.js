baadfe180d59bdaec0887d0023b15a95b13c830a
Mubiru Patrick <mubirupatrick2040@gmail.com>
2020-02-27 11:42:05 +0300

    Updates

---
+++ precache-manifest.83ac280835bc27062cac235968a8bcda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42dc5ebf237c0d734c2b68fe18a06165",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "3aabc7946dd8f9bce446",
+    "url": "/react-tutorial/static/css/main.a6c49b6d.chunk.css"
+  },
+  {
+    "revision": "d413d22c7e13ac72e2bd",
+    "url": "/react-tutorial/static/js/2.b3f31d5e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-tutorial/static/js/2.b3f31d5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aabc7946dd8f9bce446",
+    "url": "/react-tutorial/static/js/main.f71bf723.chunk.js"
+  },
+  {
+    "revision": "263d11e31e468962bfa6",
+    "url": "/react-tutorial/static/js/runtime-main.21d1fe89.js"
+  }
+]);
